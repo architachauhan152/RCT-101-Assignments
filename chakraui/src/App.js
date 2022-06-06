2ac6e6787d07d17b5b060546e0a153bbfa02a267
Ritu Chauhan <93373957+architachauhan152@users.noreply.github.com>
2022-06-06 22:49:05 +0530

    working

---
+++ chakraui/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Payment from './components/Payment';
+
+import Slide from './components/Slide';
+
+function App() {
+  return (
+    <div className="App">
+     <Slide/>
+     <Payment/>
+     {/* <Sidebar/> */}
+    </div>
+  );
+}
+
+export default App;
